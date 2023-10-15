86509f2022faa8bf30bc9b23f09ae4bebec74103
K3vX21 <54183095+kvnshu@users.noreply.github.com>
2023-10-14 21:55:20 -0400

    refine the query to only user's save

--- components/ReadingList.js
+++ components/ReadingList.js
@@ -2,19 +2,31 @@ import React from "react";
 import ReadingItemList from "./ReadingListItem";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import { spawn } from "child_process";
 
 const ReadingList = async () => {
-
   const cookieStore = cookies()
   const supabase = createServerComponentClient({ cookies: () => cookieStore })
 
-  const { data: papers } = await supabase.from('paper').select('*')
+  const { data: session } = await supabase.auth.getUser();
+  const { data: papers } = await supabase.from('save').select(`
+    user_id,
+    paper (
+      url,
+      created_at
+    )
+  `).eq('user_id', session.user.id)
 
   return (
     <div>
-      {papers.map((paper) => <ReadingItemList paper={paper} />)}
+      {papers ? (
+        papers.map((paper) => <ReadingItemList key={paper.id} paper={paper} />)
+      ) : (
+        null
+      )
+      }
     </div>
-);
+  );
 };
 
 export default ReadingList;
